731e16af0d3d41923dea97ba9b5234a16a4736b3
CyprianTinasheAarons <cyprianaarons263@gmail.com>
2022-08-05 10:13:34 +0200

    updated

--- src/components/Rarities.tsx
+++ src/components/Rarities.tsx
@@ -6,7 +6,7 @@ export function Rarities() {
                 <div className="flex flex-wrap pt-24 -m-4">
                     <div className="p-4 lg:w-1/4 md:w-1/2">
                         <div className="flex flex-col items-center h-full p-4 text-center bg-[#B9D6EC] border-4 border-black rounded-xl">
-                            <img alt="team" className="flex-shrink-0 object-cover object-center w-full mb-4 rounded-lg h-98" src="./types/fish.jpeg" />
+                            <img alt="team" className="flex-shrink-0 object-cover object-center w-full mb-4 rounded-lg h-98" src="./types/Qute.jpeg" />
                             <div className="w-full">
 
                                 <div className="flex items-center justify-between align-middle">
@@ -24,17 +24,17 @@ export function Rarities() {
                     </div>
                     <div className="p-4 lg:w-1/4 md:w-1/2">
                         <div className="flex flex-col items-center h-full p-4 text-center bg-[#B9D6EC] border-4 border-black rounded-xl">
-                            <img alt="team" className="flex-shrink-0 object-cover object-center w-full mb-4 rounded-lg h-98" src="./types/fish.jpeg" />
+                            <img alt="team" className="flex-shrink-0 object-cover object-center w-full mb-4 rounded-lg h-98" src="./types/Epic.jpeg" />
                             <div className="w-full">
 
                                 <div className="flex items-center justify-between align-middle">
-                                    <p className="mb-4 text-2xl">056</p>
-                                    <p className="mb-4 text-xl rounded-full bg-[#3AB6FF] text-white p-2">Qute</p>
+                                    <p className="mb-4 text-2xl">147</p>
+                                    <p className="mb-4 text-xl rounded-full bg-[#3AB6FF] text-white p-2">Epic</p>
 
                                 </div>
                                 <div className="flex justify-between">
                                     <p className="mb-4 text-2xl">QTY</p>
-                                    <p className="mb-4 text-2xl">408</p>
+                                    <p className="mb-4 text-2xl">312</p>
 
                                 </div>
                             </div>
@@ -42,17 +42,17 @@ export function Rarities() {
                     </div>
                     <div className="p-4 lg:w-1/4 md:w-1/2">
                         <div className="flex flex-col items-center h-full p-4 text-center bg-[#B9D6EC] border-4 border-black rounded-xl">
-                            <img alt="team" className="flex-shrink-0 object-cover object-center w-full mb-4 rounded-lg h-98" src="./types/fish.jpeg" />
+                            <img alt="team" className="flex-shrink-0 object-cover object-center w-full mb-4 rounded-lg h-98" src="./types/Rare.jpeg" />
                             <div className="w-full">
 
                                 <div className="flex items-center justify-between align-middle">
-                                    <p className="mb-4 text-2xl">056</p>
-                                    <p className="mb-4 text-xl rounded-full bg-[#3AB6FF] text-white p-2">Qute</p>
+                                    <p className="mb-4 text-2xl">038</p>
+                                    <p className="mb-4 text-xl rounded-full bg-[#3AB6FF] text-white p-2">Rare</p>
 
                                 </div>
                                 <div className="flex justify-between">
                                     <p className="mb-4 text-2xl">QTY</p>
-                                    <p className="mb-4 text-2xl">408</p>
+                                    <p className="mb-4 text-2xl">179</p>
 
                                 </div>
                             </div>
@@ -60,17 +60,17 @@ export function Rarities() {
                     </div>
                     <div className="p-4 lg:w-1/4 md:w-1/2">
                         <div className="flex flex-col items-center h-full p-4 text-center bg-[#B9D6EC] border-4 border-black rounded-xl">
-                            <img alt="team" className="flex-shrink-0 object-cover object-center w-full mb-4 rounded-lg h-98" src="./types/fish.jpeg" />
+                            <img alt="team" className="flex-shrink-0 object-cover object-center w-full mb-4 rounded-lg h-98" src="./types/Legendary.jpeg" />
                             <div className="w-full">
 
                                 <div className="flex items-center justify-between align-middle">
-                                    <p className="mb-4 text-2xl">056</p>
-                                    <p className="mb-4 text-xl rounded-full bg-[#3AB6FF] text-white p-2">Qute</p>
+                                    <p className="mb-4 text-2xl">004</p>
+                                    <p className="mb-4 text-xl rounded-full bg-[#3AB6FF] text-white p-2">Legendary</p>
 
                                 </div>
                                 <div className="flex justify-between">
                                     <p className="mb-4 text-2xl">QTY</p>
-                                    <p className="mb-4 text-2xl">408</p>
+                                    <p className="mb-4 text-2xl">99</p>
 
                                 </div>
                             </div>
--- src/components/Types.tsx
+++ src/components/Types.tsx
@@ -6,10 +6,24 @@ export function Types() {
                 <div className="flex flex-wrap pt-24 -m-4">
                     <div className="p-4 lg:w-1/4 md:w-1/2">
                         <div className="flex flex-col items-center h-full p-4 text-center bg-white border-4 border-black rounded-xl">
-                            <img alt="team" className="flex-shrink-0 object-cover object-center w-full mb-4 rounded-lg h-98" src="./types/fish.jpeg"/>
+                            <img alt="team" className="flex-shrink-0 object-cover object-center w-full mb-4 rounded-lg h-98" src="./types/candies.jpeg"/>
                                 <div className="w-full">
                                     
-                                <h3 className="mb-3 text-2xl uppercase">Fish</h3>
+                                <h3 className="mb-3 text-2xl uppercase">Candies</h3>
+                                <div className="flex justify-between">
+                                    <p className="mb-4 text-2xl">QTY</p>
+                                    <p className="mb-4 text-2xl">33</p>
+
+                                    </div>
+                                </div>
+                        </div>
+                    </div>
+                  <div className="p-4 lg:w-1/4 md:w-1/2">
+                        <div className="flex flex-col items-center h-full p-4 text-center bg-white border-4 border-black rounded-xl">
+                            <img alt="team" className="flex-shrink-0 object-cover object-center w-full mb-4 rounded-lg h-98" src="./types/dragons.jpeg"/>
+                                <div className="w-full">
+                                    
+                                <h3 className="mb-3 text-2xl uppercase">Dragons</h3>
                                 <div className="flex justify-between">
                                     <p className="mb-4 text-2xl">QTY</p>
                                     <p className="mb-4 text-2xl">66</p>
@@ -34,27 +48,13 @@ export function Types() {
                     </div>
                   <div className="p-4 lg:w-1/4 md:w-1/2">
                         <div className="flex flex-col items-center h-full p-4 text-center bg-white border-4 border-black rounded-xl">
-                            <img alt="team" className="flex-shrink-0 object-cover object-center w-full mb-4 rounded-lg h-98" src="./types/fish.jpeg"/>
+                            <img alt="team" className="flex-shrink-0 object-cover object-center w-full mb-4 rounded-lg h-98" src="./types/birds.jpeg"/>
                                 <div className="w-full">
                                     
-                                <h3 className="mb-3 text-2xl uppercase">Fish</h3>
+                                <h3 className="mb-3 text-2xl uppercase">Birds</h3>
                                 <div className="flex justify-between">
                                     <p className="mb-4 text-2xl">QTY</p>
-                                    <p className="mb-4 text-2xl">66</p>
-
-                                    </div>
-                                </div>
-                        </div>
-                    </div>
-                  <div className="p-4 lg:w-1/4 md:w-1/2">
-                        <div className="flex flex-col items-center h-full p-4 text-center bg-white border-4 border-black rounded-xl">
-                            <img alt="team" className="flex-shrink-0 object-cover object-center w-full mb-4 rounded-lg h-98" src="./types/fish.jpeg"/>
-                                <div className="w-full">
-                                    
-                                <h3 className="mb-3 text-2xl uppercase">Fish</h3>
-                                <div className="flex justify-between">
-                                    <p className="mb-4 text-2xl">QTY</p>
-                                    <p className="mb-4 text-2xl">66</p>
+                                    <p className="mb-4 text-2xl">99</p>
 
                                     </div>
                                 </div>
@@ -64,13 +64,13 @@ export function Types() {
                 <div className="flex flex-wrap pt-16 -m-4">
                   <div className="p-4 lg:w-1/4 md:w-1/2">
                         <div className="flex flex-col items-center h-full p-4 text-center bg-white border-4 border-black rounded-xl">
-                            <img alt="team" className="flex-shrink-0 object-cover object-center w-full mb-4 rounded-lg h-98" src="./types/fish.jpeg"/>
+                            <img alt="team" className="flex-shrink-0 object-cover object-center w-full mb-4 rounded-lg h-98" src="./types/frogs.jpeg"/>
                                 <div className="w-full">
                                     
-                                <h3 className="mb-3 text-2xl uppercase">Fish</h3>
+                                <h3 className="mb-3 text-2xl uppercase">Frogs</h3>
                                 <div className="flex justify-between">
                                     <p className="mb-4 text-2xl">QTY</p>
-                                    <p className="mb-4 text-2xl">66</p>
+                                    <p className="mb-4 text-2xl">133</p>
 
                                     </div>
                                 </div>
@@ -78,13 +78,13 @@ export function Types() {
                     </div>
                   <div className="p-4 lg:w-1/4 md:w-1/2">
                         <div className="flex flex-col items-center h-full p-4 text-center bg-white border-4 border-black rounded-xl">
-                            <img alt="team" className="flex-shrink-0 object-cover object-center w-full mb-4 rounded-lg h-98" src="./types/fish.jpeg"/>
+                            <img alt="team" className="flex-shrink-0 object-cover object-center w-full mb-4 rounded-lg h-98" src="./types/bunnies.jpeg"/>
                                 <div className="w-full">
                                     
-                                <h3 className="mb-3 text-2xl uppercase">Fish</h3>
+                                <h3 className="mb-3 text-2xl uppercase">Bunnies</h3>
                                 <div className="flex justify-between">
                                     <p className="mb-4 text-2xl">QTY</p>
-                                    <p className="mb-4 text-2xl">66</p>
+                                    <p className="mb-4 text-2xl">99</p>
 
                                     </div>
                                 </div>
@@ -92,13 +92,13 @@ export function Types() {
                     </div>
                   <div className="p-4 lg:w-1/4 md:w-1/2">
                         <div className="flex flex-col items-center h-full p-4 text-center bg-white border-4 border-black rounded-xl">
-                            <img alt="team" className="flex-shrink-0 object-cover object-center w-full mb-4 rounded-lg h-98" src="./types/fish.jpeg"/>
+                            <img alt="team" className="flex-shrink-0 object-cover object-center w-full mb-4 rounded-lg h-98" src="./types/astronauts.jpeg"/>
                                 <div className="w-full">
                                     
-                                <h3 className="mb-3 text-2xl uppercase">Fish</h3>
+                                <h3 className="mb-3 text-2xl uppercase">Astronauts</h3>
                                 <div className="flex justify-between">
                                     <p className="mb-4 text-2xl">QTY</p>
-                                    <p className="mb-4 text-2xl">66</p>
+                                    <p className="mb-4 text-2xl">204</p>
 
                                     </div>
                                 </div>
@@ -106,13 +106,13 @@ export function Types() {
                     </div>
                   <div className="p-4 lg:w-1/4 md:w-1/2">
                         <div className="flex flex-col items-center h-full p-4 text-center bg-white border-4 border-black rounded-xl">
-                            <img alt="team" className="flex-shrink-0 object-cover object-center w-full mb-4 rounded-lg h-98" src="./types/fish.jpeg"/>
+                            <img alt="team" className="flex-shrink-0 object-cover object-center w-full mb-4 rounded-lg h-98" src="./types/dogs.jpeg"/>
                                 <div className="w-full">
                                     
-                                <h3 className="mb-3 text-2xl uppercase">Fish</h3>
+                                <h3 className="mb-3 text-2xl uppercase">Dogs</h3>
                                 <div className="flex justify-between">
                                     <p className="mb-4 text-2xl">QTY</p>
-                                    <p className="mb-4 text-2xl">66</p>
+                                    <p className="mb-4 text-2xl">133</p>
 
                                     </div>
                                 </div>
@@ -122,13 +122,13 @@ export function Types() {
                 <div className="flex justify-center pt-16 -m-4">
                   <div className="p-4 lg:w-1/4 md:w-1/2">
                         <div className="flex flex-col items-center h-full p-4 text-center bg-white border-4 border-black rounded-xl">
-                            <img alt="team" className="flex-shrink-0 object-cover object-center w-full mb-4 rounded-lg h-98" src="./types/fish.jpeg"/>
+                            <img alt="team" className="flex-shrink-0 object-cover object-center w-full mb-4 rounded-lg h-98" src="./types/robots.jpeg"/>
                                 <div className="w-full">
                                     
-                                <h3 className="mb-3 text-2xl uppercase">Fish</h3>
+                                <h3 className="mb-3 text-2xl uppercase">Robots</h3>
                                 <div className="flex justify-between">
                                     <p className="mb-4 text-2xl">QTY</p>
-                                    <p className="mb-4 text-2xl">66</p>
+                                    <p className="mb-4 text-2xl">166</p>
 
                                     </div>
                                 </div>
